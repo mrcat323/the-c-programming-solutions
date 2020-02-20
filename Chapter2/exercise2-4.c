71f6f89f5b2eee660341dc2c450186147634f82f
Mr. CaT <mrcat323@gmail.com>
2020-02-20 23:24:37 +0500

    Updated
    Squeeze exercise, updated with new code

--- Chapter2/exercise2-4.c
+++ Chapter2/exercise2-4.c
@@ -1,13 +1,28 @@
-// squeeze: deletes each character in s1 that matches any character in the string s2.
-void squeeze(char s1[ ], char s2[ ])
-{
-    int i, j, k;
+#include <stdio.h>
 
-    for (i = j = 0; s1[i] != '\0'; ++i) {
-        for (k = 0; (s2[k] != '\0') && (s1[i] != s2[k]) ; ++k)
-            ;
-        if (s2[k] == '\0')
-            s1[j++] = s1[i];
-    }
-    s1[j] = '\0';
+void squeeze(char s1[], char s2[]);
+
+int main(void)
+{
+  char s1[] = "bad boys for life";
+  char s2[] = "bad ";
+
+  squeeze(s1, s2);
+
+  /* output: oysforlife */
+  printf("%s", s1);
+  return 0;
+}
+
+void squeeze(char s1[], char s2[])
+{
+  int i, j, k;
+
+  for (i = 0; s2[i] != '\0'; ++i) {
+    for (j = k = 0; s1[k] != '\0'; ++j)
+      if (s1[j] != s2[i])
+	s1[k++] = s1[j];
+    
+    s1[k] = '\0';
+  }
 }
