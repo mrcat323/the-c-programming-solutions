8f6e30afdcbc145a5ebad915c8b9b636dd9ddd28
Mr. CaT <mrcat323@gmail.com>
2020-02-27 22:29:17 +0500

    Updated
    Exercise 2-7 from Chapter II: invert program had been updated

--- Chapter2/exercise2-7.c
+++ Chapter2/exercise2-7.c
@@ -1,5 +1,15 @@
+#include <stdio.h>
+
+unsigned invert(unsigned, int, int);
+
+int main(void)
+{
+  /* the result is 195 (1100 0011) */
+  printf("%u", invert(255, 5, 4));
+  return 0;
+}
 /* invert: returns x with the n bits that begin at position p inverted */
 unsigned invert(unsigned x, int p, int n)
 {
-    return x ^ ((~(~0 << n)) << (p+1-n));
+  return x ^ (~(~0 << n) << p + 1 - n);
 }
