62e9d2260b4c14a0175e4b160a25a228b84edebb
Mr. CaT <mrcat323@gmail.com>
2020-03-01 00:01:19 +0500

    Updated
    Divided program into a couple of parts, refactored

--- Chapter2/exercise2-10.c
+++ Chapter2/exercise2-10.c
@@ -1,5 +1,28 @@
+#include <stdio.h>
+
+int is_upper(int);
+int lower(int);
+
+int main(void)
+{
+  /* we want to lowercase capitalized chars */
+  char s[] = "Hello World!";
+
+  int i;
+
+  for (i = 0; s[i] != '\0'; ++i)
+    s[i] = lower(s[i]);
+
+  /* which prints "hello world" */
+  printf("%s", s);
+}
+
+int is_upper(int c)
+{
+  return c >= 'A' && c <= 'Z';
+}
 /* lower: convert c to lower case; ASCII only */
 int lower(int c)
 {
-    return (c >= 'A' && c <= 'Z') ? (c + 'a' - 'A') : c;
+  return is_upper(c) ? c + 'a' - 'A' : c;
 }
