b741e63f9e1696feb6932f8f46e3610028122c89
Mr. CaT <mrcat323@gmail.com>
2020-03-01 00:00:51 +0500

    Updated
    Exercise 2-9 updated

--- Chapter2/exercise2-9.c
+++ Chapter2/exercise2-9.c
@@ -1,10 +1,21 @@
+#include <stdio.h>
+
+int bitcount(unsigned);
+
+int main(void)
+{
+  /* suppose we want to count 1 bits of 255 */
+  printf("%d", bitcount(255));
+
+  return 0;
+}
 /* bitcount: count 1 bits in x */
 int bitcount(unsigned x)
 {
-    int b;
+  int b;
 
-    for (b = 0; x != 0; x &= (x-1))
-        b++;
+  for (b = 0; x != 0; x &= (x - 1), ++b)
+    ;
 
-    return b;
+  return b;
 }
